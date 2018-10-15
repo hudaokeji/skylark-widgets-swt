eefcd8a4337bf347ed2914770ba172f75a934a12
liuxiaofang <liuxiaofang0716@gmail.com>
2018-10-15 22:22:34 +0800

    Modify each package to use external tools

--- dist/uncompressed/skylark-ui-swt.js
+++ dist/uncompressed/skylark-ui-swt.js
@@ -7,9 +7,9 @@
  */
 (function(factory,globals) {
   var define = globals.define,
-  	  require = globals.require,
-  	  isAmd = (typeof define === 'function' && define.amd),
-  	  isCmd = (!isAmd && typeof exports !== 'undefined');
+      require = globals.require,
+      isAmd = (typeof define === 'function' && define.amd),
+      isCmd = (!isAmd && typeof exports !== 'undefined');
 
   if (!isAmd && !define) {
     var map = {};
@@ -18,7 +18,7 @@
           return relative;
         }
         var stack = base.split("/"),
-            parts = relative.split("/");    
+            parts = relative.split("/");
         stack.pop(); 
         for (var i=0; i<parts.length; i++) {
             if (parts[i] == ".")
@@ -41,7 +41,7 @@
             };
             require(id);
         } else {
-            resolved[id] = factory;
+            map[id] = factory;
         }
     };
     require = globals.require = function(id) {
@@ -61,17 +61,20 @@
         return module.exports;
     };
   }
+  
+  if (!define) {
+     throw new Error("The module utility (ex: requirejs or skylark-utils) is not loaded!");
+  }
 
   factory(define,require);
 
-  if (!isAmd) { 
-    if (isCmd) {
-      exports = require("skylark-bootstrap3/main");
-    } else {
-      if (!globals.skylarkjs) {
-         globals.skylarkjs = require("skylark-langx/skylark");
-      }
+  if (!isAmd) {
+    var skylarkjs = require("skylark-langx/skylark");
 
+    if (isCmd) {
+      exports = skylarkjs;
+    } else {
+      globals.skylarkjs  = skylarkjs;
     }
   }
 
--- dist/uncompressed/skylark-ui-swt/main.js
+++ dist/uncompressed/skylark-ui-swt/main.js
@@ -1,10 +1,3 @@
-/**
- * skylark-ui-swt - A version of ui-swt that ported to running on skylarkjs
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylarkui/skylark-ui-swt/
- * @license MIT
- */
 define([
     "./ui",
     "./Widget"
