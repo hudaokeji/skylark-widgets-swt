d976a5c2fb1810977b0f54df7321e7f02786cdd2
hudaokeji <github@hudaokeji.com>
2019-10-13 17:36:04 +0900

    update Listing.js

--- dist/uncompressed/skylark-widgets-swt-all.js
+++ dist/uncompressed/skylark-widgets-swt-all.js
@@ -11560,6 +11560,7 @@ define('skylark-widgets-base/Widget',[
     },
 
 
+
     /**
      * Returns a parent widget  enclosing this widgets, or null if not exist.
      *
@@ -13766,21 +13767,30 @@ define('skylark-widgets-swt/TextBox',[
             mode   : "",  // "tree" or "accordion" or "popup"
             levels : 2,
             selectors :  {
-              children : "> ul",  // "> .list-group"
+              children : "ul",  // "> .list-group"
               hasChildren : ":has(ul)"
             },
-            classes : {
-              expandIcon: 'glyphicon glyphicon-plus',    // "glyphicon-chevron-down"
-              collapseIcon: 'glyphicon glyphicon-minus', // "glyphicon-chevron-right"
-              children : ""                              // "list-group children"
-            },
-            templates : {
-              tree : {
-                item : "<span><i class=\"glyphicon\"></i><a href=\"javascript: void(0);\"></a> </span>",
+            tree : {
+              classes : {
+                expandIcon: 'glyphicon-plus',    // "glyphicon-chevron-down", 'glyphicon-folder-open'
+                collapseIcon: 'glyphicon-minus', // "glyphicon-chevron-right", 'glyphicon-folder-close'
+                children : ""                              // "list-group children"
+              },
+              templates : {
+                treeIcon : "<i class=\"glyphicon\"></i>",
                 itemGroup: ""
 
+              },
+              selectors : {
+                treeIcon : " > i"
               }
-                            
+            },
+
+            accordion : {
+              selectors : {
+                toggler : " > a"
+              }
+
             }
           },
 
@@ -13788,9 +13798,22 @@ define('skylark-widgets-swt/TextBox',[
         	classes : {
           	active : "active"
         	},
+
+
         	selectors : {
-          	item : "li"                   // ".list-group-item"
+          	item : "li",                   // ".list-group-item"
+
         	},
+
+          item : {
+            template : "<span><i class=\"glyphicon\"></i><a href=\"javascript: void(0);\"></a> </span>",
+            checkable : false,
+            selectors : {
+              icon : " > span > i",
+              text : " > span > a"
+            }
+          },
+
         	selected : 0
         },
 
@@ -13825,51 +13848,73 @@ define('skylark-widgets-swt/TextBox',[
             var $this = velm,
                 $toggle = this.options.toggle,
                 multiTierMode = this.options.multiTier.mode,
+                hasChildrenSelector = this.options.multiTier.selectors.hasChildren,
+                childrenSelector = this.options.multiTier.selectors.children,
+                iconSelector = this.options.item.selectors.icon,
+                textSelector = this.options.item.selectors.text,
+                itemTemplate = this.options.item.template,                
                 obj = this;
 
-            if (multiTierMode == "tree") {
-                 $this.query(this.options.selectors.item).each(function(){
-                   if($(this).is(self.options.multiTier.selectors.hasChildren)) {
-                      var children = $(this).find(' > ul');
-                      $(children).remove();
-                      text = $(this).text().trim();
-                      $(this).html(self.options.multiTier.templates.tree.item);
-                      $(this).find(' > span > i').addClass('glyphicon-folder-open');
-                      $(this).find(' > span > a').text(text);
-                      $(this).append(children);
-                    }  else {
-                      text = $(this).text().trim();
-                      $(this).html(self.options.multiTier.templates.tree.item);
-                      $(this).find(' > span > i').addClass('glyphicon-file');
-                      $(this).find(' > span > a').text(text);
-                  }
 
-                 });
-            }
+            if (multiTierMode) {
+              if (multiTierMode == "tree") {
+                   var treeIconTemplate = this.options.multiTier.tree.templates.treeIcon,
+                       treeIconSelector = this.options.multiTier.tree.selectors.treeIcon,
+                       expandIconClass = this.options.multiTier.tree.classes.expandIcon,
+                       collapseIconClass = this.options.multiTier.tree.classes.collapseIcon;
 
-            //if (this.isIE() <= 9) {
-            //    $this.find("li.active").has("ul").children("ul").collapse("show");
-            //    $this.find("li").not(".active").has("ul").children("ul").collapse("hide");
-            //} else {
-                $this.query("li.active").has("ul").children("ul").addClass("collapse in");
-                $this.query("li").not(".active").has("ul").children("ul").addClass("collapse");
-            //}
+                   this._$items.each(function(){
+                     if($(this).is(hasChildrenSelector)) {
+                        var children = $(this).find(childrenSelector);
+                        $(children).remove();
+                        text = $(this).text().trim();
+                        $(this).html(treeIconTemplate+itemTemplate);
+                        $(this).find(treeIconSelector).addClass(expandIconClass).on("click" + "." + self.pluginName, function(e) {
+                            e.preventDefault();
 
+                            $(this).toggleClass(expandIconClass).toggleClass(collapseIconClass);
 
+                            $(this).closest("li").toggleClass("active").children("ul").collapse("toggle");
 
+                            if ($toggle) {
+                                $(this).closest("li").siblings().removeClass("active").children("ul.in").collapse("hide");
+                            }
+                        });
 
-            $this.query("li").has("ul").find("a").on("click" + "." + this.pluginName, function(e) {
-                e.preventDefault();
-
-                $(this).closest("li").toggleClass("active").children("ul").collapse("toggle");
-
-                if ($toggle) {
-                    $(this).closest("li").siblings().removeClass("active").children("ul.in").collapse("hide");
-                }
-
-            });
+                        $(this).find(iconSelector).addClass('glyphicon-folder-open');
+                        $(this).find(textSelector).text(text);
+                        $(this).append(children);
 
 
+
+                      }  else {
+                        text = $(this).text().trim();
+                        $(this).html(treeIconTemplate+itemTemplate);
+                        $(this).find(iconSelector).addClass('glyphicon-file');
+                        $(this).find(textSelector).text(text);
+                    }
+
+                   });
+              } else if (multiTierMode == "accordion") {
+                var togglerSelector = self.options.multiTier.accordion.selectors.toggler;
+
+                this._$items.has(childrenSelector).find(togglerSelector).on("click" + "." + this.pluginName, function(e) {
+                    e.preventDefault();
+
+                    $(this).closest(itemSelector).toggleClass("active").children(childrenSelector).collapse("toggle");
+
+                    if ($toggle) {
+                        $(this).closest(itemSelector).siblings().removeClass("active").children(childrenSelector+".in").collapse("hide");
+                    }
+                });
+              }
+
+
+             this._$items.filter(".active").has(childrenSelector).children(childrenSelector).addClass("collapse in");
+             this._$items.not(".active").has(childrenSelector).children(childrenSelector).addClass("collapse");
+
+              
+            }   
         },
 
         _refresh : function(updates) {
